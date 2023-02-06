754f2000762414b4f6b9706a43cfd6d9cf2cd733
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-06 12:03:51 +0300

    01

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./AppComponent";
+
+function App() {
+    const shapka = 'What to learn';
+    const shapka1 = 'What to learn1';
+    const shapka2= 'What to learn111111111111111111111';
+    const shapka3 = 'What to learn2222222222222222222222222';
+
+    const tasks1 = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
+    const tasks2 = [
+        { id: 1, title: "Hello world", isDone: true },
+        { id: 2, title: "I am Happy", isDone: false },
+        { id: 3, title: "Yo", isDone: false }
+    ]
+
+
+    return (
+        <div className="App">
+            <Todolist shapkaOne = {shapka1} tasks1 = {tasks1}/>
+            <Todolist shapka = {shapka2} shapkaOne = {shapka3} tasks1 = {tasks2}/>
+        </div>
+    );
+}
+
+export default App;
