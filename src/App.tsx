098cf57de9a7903c6cbded112fad834b7e2a8f59
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-21 23:01:58 +0300

    04

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import {Todolist} from './Todolist';
 import { v1 } from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
+
 function App() {
+
     let [tasks, setTasks] = useState([
         { id: v1(), title: "HTML&CSS", isDone: true },
         { id: v1(), title: "JS", isDone: true },
@@ -12,15 +14,18 @@ function App() {
         { id: v1(), title: "Rest API", isDone: false },
         { id: v1(), title: "GraphQL", isDone: false },
     ]);
+
     function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id != id);
         setTasks(filteredTasks);
     }
+
     function addTask(title: string) {
         let task = { id: v1(), title: title, isDone: false };
         let newTasks = [task, ...tasks];
         setTasks(newTasks);
     }
+
     let [filter, setFilter] = useState<FilterValuesType>("all");
     let tasksForTodolist = tasks;
     if (filter === "active") {
