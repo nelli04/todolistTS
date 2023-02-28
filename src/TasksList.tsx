90c8f206fe4f6bd262f5843920f980138a726525
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-28 21:43:28 +0300

    03

---
+++ src/TasksList.tsx
@@ -0,0 +1,39 @@
+import React, {ChangeEvent, FC} from 'react';
+import {TaskType} from "./TodoList";
+
+type TasksListPropsType = {
+    tasksID: string
+    tasks: TaskType[]
+    removeTask: (taskId: string, tasksID: string) => void
+    changeTaskStatus: (taskId: string, isDone: boolean, tasksID: string) => void
+}
+
+const TasksList: FC<TasksListPropsType> = (props): JSX.Element => {
+    const tasksItems: JSX.Element[] | JSX.Element =
+        props.tasks.length
+        ? props.tasks.map((task) => {
+            const taskClasses = task.isDone ? "task task-done" : "task"
+            const removeTaskHandler = () => props.removeTask(task.id, props.tasksID)
+            const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) =>
+                props.changeTaskStatus(task.id, e.currentTarget.checked, props.tasksID)
+            return (
+                <li key={task.id}>
+                    <input
+                        type="checkbox"
+                        checked={task.isDone}
+                        onChange={changeTaskStatusHandler}
+                    />
+                    <span className={taskClasses}>{task.title}</span>
+                    <button onClick={removeTaskHandler}>x</button>
+                </li>
+            )
+        })
+        : <span>Your taskslist is empty</span>
+    return (
+        <ul>
+            {tasksItems}
+        </ul>
+    );
+};
+
+export default TasksList;
